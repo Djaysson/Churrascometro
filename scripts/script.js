047bab9f9cab994044bd7b8474b4d86ef4f5781e
Djaysson <djayson.dev@gmail.com>
2021-04-01 18:13:51 -0300

    Assembling the initial HTML structure of the project

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+//regras de negocio//
+//Carne - 400 gr por pessoa + de 6horas - 650
+//Cerveja - 1200 ml por pessoa  + 6horas 2000ml
+// Refrigerante /agua =  1000ml por pessoa + 6hora 1500ml
+
+// crianças valem por 0,5
